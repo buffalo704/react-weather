beda99a905c4792dfb2a930bc9564e4812ec4b46
Sid <buffalo704@hotmail.com>
2017-05-08 23:55:24 -0400

    sparklines with labels

---
+++ src/components/chart.js
@@ -0,0 +1,22 @@
+/**
+ * Created by sso on 5/8/17.
+ */
+import React, { Component } from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
+
+const average = (data) => {
+        return _.round(_.sum(data)/data.length);
+}
+
+export default (props) => {
+    return (
+        <div>
+            <Sparklines height={120} width={180} data={props.data}>
+                <SparklinesLine color={props.color}/>
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>{average(props.data)} {props.units}</div>
+        </div>
+    );
+}
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,7 +3,7 @@
  */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class Weatherlist extends Component {
 
@@ -11,15 +11,13 @@ class Weatherlist extends Component {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
     const humidities  = cityData.list.map(weather => weather.main.humidity);
-    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
       return (
           <tr key={name}>
               <td>{name}</td>
-              <td>
-                  <Sparklines height={120} width={180} data={temps}>
-                      <SparklinesLine color="red"/>
-                  </Sparklines>
-              </td>
+              <td className="fixed"><Chart data={temps} color="orange" units="K" /></td>
+              <td className="fixed"><Chart data={pressures} color="green" units="hPa" /></td>
+              <td className="fixed"><Chart data={humidities} color="black" units="%" /></td>
           </tr>
       )
     };
@@ -30,9 +28,9 @@ class Weatherlist extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
